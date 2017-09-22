7a098814e5921a2486c26f4947f8462f091a35c2
Daniel Chen <danielchenphhs@gmail.com>
2017-09-22 15:11:14 -0500

    setting up new file structure

---
+++ js/oldMain.js
@@ -0,0 +1,70 @@
+var electoralMap;
+
+var white;
+var black;
+var hispanic;
+var asian;
+
+var dataCSV = {};
+
+var dateFormatter = d3.time.format("%d-%b-%Y");
+
+loadData();
+
+function loadData() {
+
+    queue()
+        .defer(d3.csv, "data/TestStateValues.csv")
+        .await(function(error, CSV) {
+            CSV.forEach(function(d) {
+                d.Population = +d.Population;
+                d.PercentTotalPopulation = +d.PercentTotalPopulation;
+                d.ElectoralVotes = +d.ElectoralVotes;
+                d.PercentElectoralVotes = +d.PercentElectoralVotes;
+                d.ElectoralToPopRatio = +d.ElectoralToPopRatio;
+            });
+
+            CSV.forEach(function(d,index) {
+                dataCSV[d.Location] = d;
+            });
+            createVis();
+        });
+}
+
+
+function createVis() {
+
+    var EventHandler = {};
+    electoralMap = new OldElectoralMap("#ElectoralMap",dataCSV,EventHandler);
+
+    var statePinned = false;
+    var pinned = false;
+    //
+    $(EventHandler).bind("stateOver", function(event, state){
+        if (!pinned) {
+            electoralMap.highlightState(state);
+        }
+    });
+    $(EventHandler).bind("stateOff", function(event){
+        if (!pinned) {
+            electoralMap.highlightState(null);
+        }
+    //
+    });
+    $(EventHandler).bind("press", function(event, state){
+        if (pinned) {
+            electoralMap.highlightState(null);
+            statePinned = false;
+            pinned = false;
+        } else {
+            electoralMap.highlightState(state);
+            statePinned = true;
+            pinned = true;
+        }
+    });
+
+    white = new OldPerson("#white-picture",1.034, 151141962,"White");
+    black = new OldPerson("#black-picture",1.004, 29732845,"Black");
+    hispanic = new OldPerson("#hisp-picture",.92, 44599268,"Hispanic");
+    asian = new OldPerson("#asian-picture",.88, 14866423,"Asian");
+}
---
+++ js/oldPerson.js
@@ -0,0 +1,83 @@
+/*
+ *  Person - Object constructor function
+ *  @param _parentElement   -- HTML element in which to draw the visualization
+ *  @param _data            -- Array with all stations of the bike-sharing network
+ */
+
+Person = function(_parentElement, _votePower, _rawPopulation, _race) {
+
+    this.parentElement = _parentElement;
+    this.votePower = _votePower;
+    this.rawPopulation = _rawPopulation;
+    this.race = _race;
+
+    this.initVis();
+};
+
+Person.prototype.initVis = function() {
+
+    var vis = this;
+
+    vis.formatValue = d3.format(",.0f");
+
+    vis.imgWidth = 329;
+    vis.imgHeight = 834;
+
+    vis.margin = {top: 0, right: 0, bottom: 0, left: 0};
+    vis.width = $(vis.parentElement).width() - vis.margin.left - vis.margin.right;
+    vis.height = 150 - vis.margin.top - vis.margin.bottom;
+
+
+    vis.svg = d3.select(vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+    vis.personHeight = 100;
+
+
+    vis.tip = d3.tip().attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "There are " + vis.formatValue(vis.rawPopulation) + " " + vis.race + " Americans, but in a presidential election, <br>their votes count for " + vis.formatValue(vis.rawPopulation * vis.votePower) + " as compared to a one-person-one-vote system.";
+        });
+
+    vis.svg.call(vis.tip);
+
+    vis.wrangleData();
+
+};
+
+Person.prototype.wrangleData = function() {
+    var vis = this;
+
+    vis.updateVis();
+
+};
+
+Person.prototype.updateVis = function() {
+    var vis = this;
+
+    vis.bgImage = vis.svg
+        .append("image")
+        .attr("class", "bg-image")
+        .attr("height", vis.personHeight)
+        .attr("x", vis.width/2 - 50)
+        .attr("y", vis.height/2 - vis.personHeight/2)
+        .attr("xlink:href", "img/personfull.png")
+        .attr("opacity",.5);
+
+
+    vis.frImage = vis.svg
+        .append("image")
+        .attr("class", "fr-image")
+        .attr("height", vis.personHeight * vis.votePower)
+        .attr("x", vis.width/2)
+        .attr("y", vis.height/2 - vis.personHeight/2 - (vis.personHeight * vis.votePower - vis.personHeight))
+        .attr("xlink:href", "img/personfull.png")
+        .attr("opacity",1)
+        .on('mouseover', vis.tip.show)
+        .on('mouseout', vis.tip.hide);
+
+};
