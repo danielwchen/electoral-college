18b8d9ad6b41f2c15eba9f9b5120b714d8fa7f7b
Daniel Chen <danielchenphhs@gmail.com>
2017-09-22 16:43:03 -0500

    abstract attr selection

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
 /**
  * Created by Daniel on 7/27/17.
  */
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x, y, ypos, ybot;
+ var w = window,
+ d = document,
+ e = d.documentElement,
+ g = d.getElementsByTagName('body')[0],
+ x, y, ypos, ybot;
 
-checkWinStat()
+ checkWinStat()
 
-function checkWinStat() {
-    x = w.innerWidth || e.clientWidth || g.clientWidth;
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-    ypos = w.scrollY;
-}
+ function checkWinStat() {
+ 	x = w.innerWidth || e.clientWidth || g.clientWidth;
+ 	y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+ 	ypos = w.scrollY;
+ }
 
-$( window ).resize(function() {
-    checkWinStat();
-    electoralMap.resize();
-});
+ $( window ).resize(function() {
+ 	checkWinStat();
+ 	electoralMap.resize();
+ });
 
-var electoralMap;
+ var electoralMap;
 
 
-var EventHandler = {};
-electoralMap = new ElectoralMap("#map-vis",1,EventHandler);
+ var EventHandler = {};
+ electoralMap = new ElectoralMap("#map-vis",1,EventHandler);
+
+
+ d3.graphScroll()
+ .graph(d3.selectAll('#graph'))
+ .container(d3.select('#container'))
+ .sections(d3.selectAll('#sections > div'))
+ .on('active', function(i){ console.log(i + 'th section active') })
