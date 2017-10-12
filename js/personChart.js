f88b24d08ec5960b613a2c4688acfb05fcbb9f44
Daniel Chen <danielchenphhs@gmail.com>
2017-10-12 16:54:01 -0500

    css

--- js/personChart.js
+++ js/personChart.js
@@ -77,7 +77,7 @@ PersonChart.prototype.createVis = function() {
   vis.tipvr = d3.tip()
   .attr("class", "d3-tip")
   .offset([-8, 0])
-  .style('z-index', '999999999');
+  .style('z-index', '999999999')
   .html(function(d) { return "Voting rate: " + d.rate; });
   vis.svg.call(vis.tipvr);
 
